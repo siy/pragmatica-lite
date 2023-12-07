efbe0ce97f5538552c24904ef34570256bfe7fe2
Sergiy Yevtushenko <sergiy.yevtushenko@gmail.com>
2023-12-07 21:57:18 +0100

    Intermediate commit

--- net/http/http-client/src/main/java/org/pragmatica/http/client/HttpClient.java
+++ net/http/http-client/src/main/java/org/pragmatica/http/client/HttpClient.java
@@ -1,21 +1,19 @@
 package org.pragmatica.http.client;
 
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelFutureListener;
-import io.netty.channel.ChannelInitializer;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.*;
 import io.netty.channel.socket.SocketChannel;
-import io.netty.handler.codec.http.HttpClientCodec;
-import io.netty.handler.codec.http.HttpContentDecompressor;
-import io.netty.handler.codec.http.HttpObjectAggregator;
-import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.*;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import org.pragmatica.dns.DomainAddress;
 import org.pragmatica.dns.DomainName;
 import org.pragmatica.dns.DomainNameResolver;
 import org.pragmatica.http.HttpError;
-import org.pragmatica.http.protocol.HttpStatus;
 import org.pragmatica.lang.Promise;
+import org.pragmatica.lang.Result;
 import org.pragmatica.lang.Unit;
 import org.pragmatica.lang.utils.Causes;
 import org.pragmatica.net.InetPort;
@@ -25,30 +23,28 @@ import org.pragmatica.uri.IRI;
 import static org.pragmatica.dns.DomainName.*;
 import static org.pragmatica.http.HttpError.httpError;
 import static org.pragmatica.http.protocol.HttpStatus.NOT_IMPLEMENTED;
+import static org.pragmatica.lang.Unit.unit;
+import static org.pragmatica.lang.Unit.unitResult;
 
 //TODO: add customisation for name resolver
 @SuppressWarnings("unused")
 public interface HttpClient {
     static HttpClient create(HttpClientConfiguration configuration) {
-        var bootstrap = TransportConfiguration.transportConfiguration()
-                                              .clientBootstrap().handler(new ChannelInitializer<SocketChannel>() {
+        var bootstrap = TransportConfiguration
+            .transportConfiguration()
+            .clientBootstrap()
+            .option(ChannelOption.SO_RCVBUF, configuration.receiveBufferSize())
+            .option(ChannelOption.SO_SNDBUF, configuration.sendBufferSize())
+            .handler(new ChannelInitializer<SocketChannel>() {
                 @Override
                 public void initChannel(SocketChannel ch) {
                     var pipeline = ch.pipeline();
 
-                    // SSL is configured on per-request basis
-                    //        configuration.sslContext()
-                    //                     .onPresent(sslCtx -> pipeline.addLast(sslCtx.newHandler(ch.alloc())));
                     pipeline.addLast(new HttpClientCodec());
 
                     // TODO: consider adding support for compression
-                    pipeline.addLast(new HttpContentDecompressor());
-                    pipeline.addLast(new HttpObjectAggregator(1048576));
-                    // Individual handler is added on per-request basis
-                    //
-                    //        pipeline.addLast(new HttpSnoopClientHandler());
-
-                    // SSL is configured on per-request basis
+                    // pipeline.addLast(new HttpContentDecompressor());
+                    pipeline.addLast(new HttpObjectAggregator(configuration.maxContentLen()));
                 }
             });
 
@@ -59,7 +55,6 @@ public interface HttpClient {
 }
 
 record HttpClientImpl(HttpClientConfiguration configuration, Bootstrap bootstrap) implements HttpClient {
-
     private static final HttpError ONLY_HTTP_IS_SUPPORTED = httpError(NOT_IMPLEMENTED, "Only HTTP is supported");
     private static final HttpError ONLY_ABSOLUTE_IRI_IS_SUPPORTED = httpError(NOT_IMPLEMENTED, "Only HTTP is supported");
     private static final DomainName LOCALHOST = domainName("localhost");
@@ -84,18 +79,8 @@ record HttpClientImpl(HttpClientConfiguration configuration, Bootstrap bootstrap
     }
 
     private <T> Promise<HttpClientResponse> setupRequest(HttpClientRequest<T> request, DomainAddress address) {
-
-//        bootstrap().connect(address.ip(), calculatePort(request.iri()))
-//                   .addListener();
-
-        return connect(address, calculatePort(request.iri()));
-    }
-
-    private int calculatePort(IRI iri) {
-        return iri.port()
-                  .map(InetPort::port)
-                  .map(Integer::valueOf)
-                  .or(() -> iri.isSecure() ? 443 : 80);
+        return connect(address, calculatePort(request.iri()))
+            .flatMap(channel -> sendRequest(channel, request));
     }
 
     Promise<Channel> connect(DomainAddress address, int port) {
@@ -108,5 +93,68 @@ record HttpClientImpl(HttpClientConfiguration configuration, Bootstrap bootstrap
                     promise.failure(Causes.fromThrowable(future.cause()));
                 }
             }));
+    }
+
+    private int calculatePort(IRI iri) {
+        return iri.port()
+                  .map(InetPort::port)
+                  .map(Integer::valueOf)
+                  .or(() -> iri.isSecure() ? 443 : 80);
+    }
+
+    private static Result<SslContext> newUntrustedContext() {
+        return Result.lift(Causes::fromThrowable,
+                           () -> SslContextBuilder.forClient()
+                                                  .trustManager(InsecureTrustManagerFactory.INSTANCE)
+                                                  .build());
+    }
+
+    private <T> Promise<HttpClientResponse> sendRequest(Channel channel, HttpClientRequest<T> request) {
+        return Promise.resolved(configureConnection(channel, request.iri().isSecure()))
+                      .flatMap(_ -> Promise.promise(promise -> {
+                          channel.pipeline()
+                                 .addLast(new ContextHandler<>(promise, request));
+                          channel.write(serializeRequest(request));
+                      }));
+    }
+
+    private Result<Unit> configureConnection(Channel channel, boolean secure) {
+        if (!secure) {
+            return unitResult();
+        }
+
+        if (configuration().sslContext().isPresent()) {
+            configuration().sslContext()
+                           .onPresent(sslContext -> addSslHandler(channel, sslContext));
+            return unitResult();
+        }
+
+        return newUntrustedContext()
+            .onSuccess(sslContext -> addSslHandler(channel, sslContext))
+            .map(Unit::unit);
+    }
+
+    private static ChannelPipeline addSslHandler(Channel channel, SslContext sslContext) {
+        return channel.pipeline()
+                      .addFirst(sslContext.newHandler(channel.alloc()));
+    }
+
+    private <T> ByteBuf serializeRequest(HttpClientRequest<T> request) {
+        throw new UnsupportedOperationException();
+    }
+}
+
+class ContextHandler<T> extends SimpleChannelInboundHandler<FullHttpResponse> {
+    private final Promise<HttpClientResponse> promise;
+    private final HttpClientRequest<T> request;
+
+    public ContextHandler(Promise<HttpClientResponse> promise, HttpClientRequest<T> request) {
+        this.promise = promise;
+        this.request = request;
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, FullHttpResponse msg) throws Exception {
+        throw new UnsupportedOperationException();
     }
 }
