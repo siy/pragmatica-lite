a281750cdbcfd27030b732fd4734c8569e9d00a4
Sergiy Yevtushenko <sergiy@radixdlt.com>
2024-01-18 19:25:24 +0100

    Minor cleanup in README.md

---
+++ annotation-processor/src/main/java/org/pragmatica/annotation/Template.java
@@ -0,0 +1,8 @@
+package org.pragmatica.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.SOURCE)
+public @interface Template {
+}
