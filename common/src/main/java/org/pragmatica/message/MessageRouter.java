3dc5538bbcfa3971212b621ae42ea8b594c518c2
Sergiy Yevtushenko <sergiy.yevtushenko@gmail.com>
2025-08-04 22:35:48 +0200

    Added message router assembling with sealed hierarchy checking

--- common/src/main/java/org/pragmatica/message/MessageRouter.java
+++ common/src/main/java/org/pragmatica/message/MessageRouter.java
@@ -1,16 +1,20 @@
 package org.pragmatica.message;
 
+import org.pragmatica.lang.Cause;
 import org.pragmatica.lang.Option;
 import org.pragmatica.lang.Promise;
+import org.pragmatica.lang.Result;
+import org.pragmatica.lang.Tuple.Tuple2;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.pragmatica.lang.Tuple.tuple;
 
 /// **NOTE**: This implementation assumes that the instance is configured and then used without
 /// changes.
@@ -21,9 +25,7 @@ public interface MessageRouter {
         Promise.async(() -> route(messageSupplier.get()));
     }
 
-    <T extends Message> MessageRouter addRoute(Class<T> messageType, Consumer<T> receiver);
-
-    void validate(List<Class<? extends Message>> messageTypes);
+    <T extends Message> MessageRouter addRoute(Class<? extends T> messageType, Consumer<? extends T> receiver);
 
     static MessageRouter messageRouter() {
         @SuppressWarnings("unchecked")
@@ -35,31 +37,110 @@ public interface MessageRouter {
             @Override
             public <R extends Message> void route(R message) {
                 Option.option(routingTable.get(message.getClass()))
-                        .onPresent(list -> list.forEach(fn -> fn.accept((T) message)))
-                        .onEmpty(() -> log.warn("No route for message type: {}", message.getClass().getSimpleName()));
+                      .onPresent(list -> list.forEach(fn -> fn.accept((T) message)))
+                      .onEmpty(() -> log.warn("No route for message type: {}", message.getClass().getSimpleName()));
             }
 
             @Override
-            public <R extends Message> MessageRouter addRoute(Class<R> messageType, Consumer<R> receiver) {
+            public <R extends Message> MessageRouter addRoute(Class<? extends R> messageType, Consumer<? extends R> receiver) {
                 routingTable.computeIfAbsent((Class<T>) messageType, _ -> new ArrayList<>())
                             .add((Consumer<T>) receiver);
 
                 return this;
             }
-
-            @Override
-            public void validate(List<Class<? extends Message>> messageTypes) {
-                var missing = messageTypes.stream()
-                                          .filter(messageType -> !routingTable.containsKey(messageType))
-                                          .map(Class::getSimpleName)
-                                          .toList();
-
-                if (!missing.isEmpty()) {
-                    throw new IllegalArgumentException("Missing message types: " + String.join(", ", missing));
-                }
-            }
         }
 
         return new messageRouter<>(new HashMap<>());
     }
+
+    interface Entry<T extends Message> {
+        Stream<Tuple2<Class<? extends T>, Consumer<? extends T>>> entries();
+
+        Class<T> type();
+
+        Set<Class<?>> validate();
+
+        default Result<MessageRouter> asRouter() {
+            var validated = validate();
+
+            if (!validated.isEmpty()) {
+                var missing = validated.stream()
+                                       .map(Class::getSimpleName)
+                                       .collect(Collectors.joining(", "));
+                return new InvalidMessageRouterConfiguration("Missing message types: " + missing).result();
+            }
+
+            var router = messageRouter();
+            entries().forEach(tuple -> tuple.map(router::addRoute));
+            return Result.success(router);
+        }
+
+        interface SealedBuilder<T extends Message> extends Entry<T> {
+            static <T extends Message> SealedBuilder<T> from(Class<T> clazz) {
+                record sealedBuilder<T extends Message>(Class<T> type,
+                                                        List<Entry<? extends T>> routes) implements SealedBuilder<T> {
+                    @SuppressWarnings("unchecked")
+                    @Override
+                    public Stream<Tuple2<Class<? extends T>, Consumer<? extends T>>> entries() {
+                        return routes.stream()
+                                     .map(entry -> (Entry<T>) entry)
+                                     .flatMap(Entry::entries);
+                    }
+
+                    @SafeVarargs
+                    @Override
+                    public final Entry<T> route(Entry<? extends T>... routes) {
+                        routes().addAll(List.of(routes));
+
+                        return this;
+                    }
+
+                    @Override
+                    public Set<Class<?>> validate() {
+                        if (!type().isSealed()) {
+                            return mergeSubroutes(new HashSet<>());
+                        }
+
+                        var declared = routes().stream()
+                                               .map(Entry::type)
+                                               .collect(Collectors.toSet());
+                        var permitted = new HashSet<>(Set.of(type().getPermittedSubclasses()));
+
+                        permitted.removeAll(declared);
+
+                        return mergeSubroutes(permitted);
+                    }
+
+                    private Set<Class<?>> mergeSubroutes(Set<Class<?>> local) {
+                        routes().forEach(route -> local.addAll(route.validate()));
+                        return local;
+                    }
+                }
+
+                return new sealedBuilder<>(clazz, new ArrayList<>());
+            }
+
+            @SuppressWarnings("unchecked")
+            Entry<T> route(Entry<? extends T>... routes);
+        }
+
+        static <T extends Message> Entry<T> route(Class<T> type, Consumer<T> receiver) {
+            record entry<T extends Message>(Class<T> type, Consumer<T> receiver) implements Entry<T> {
+
+                @Override
+                public Stream<Tuple2<Class<? extends T>, Consumer<? extends T>>> entries() {
+                    return Stream.of(tuple(type(), receiver()));
+                }
+
+                @Override
+                public Set<Class<?>> validate() {
+                    return Set.of(); // Route is always valid
+                }
+            }
+
+            return new entry<>(type, receiver);
+        }
+    }
+
+    record InvalidMessageRouterConfiguration(String message) implements Cause {}
 }
