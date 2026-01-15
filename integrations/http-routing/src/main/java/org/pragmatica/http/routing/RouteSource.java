d3699992e22075551708b3bad91496ec5d3a1946
Sergiy Yevtushenko <sergiy.yevtushenko@gmail.com>
2026-01-15 15:15:50 +0100

    style: fix wildcard spacing in RouteSource

--- integrations/http-routing/src/main/java/org/pragmatica/http/routing/RouteSource.java
+++ integrations/http-routing/src/main/java/org/pragmatica/http/routing/RouteSource.java
@@ -3,11 +3,10 @@ package org.pragmatica.http.routing;
 import java.util.stream.Stream;
 
 public interface RouteSource {
-    Stream<Route< ?>> routes();
+    Stream<Route<?>> routes();
 
     default RouteSource withPrefix(String prefix) {
-        return () -> routes()
-                           .map(route -> (Route< ? >) route.withPrefix(prefix));
+        return () -> routes().map(route -> (Route<?>) route.withPrefix(prefix));
     }
 
     static RouteSource of(RouteSource... routes) {
